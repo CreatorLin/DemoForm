6efc750be45954c3582268153589e5765389ef94
Kahou <creator.kahou@gmail.com>
2016-12-07 16:45:00 +0800

    add 選項 model

---
+++ DemoForm/Models/Enums/SpecialOption.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoForm.Models
+{
+    /// <summary>
+    /// 特殊選項
+    /// </summary>
+    public enum SpecialOption
+    {
+        None,
+        以上皆是,
+        以上皆非
+    }
+}
---
+++ DemoForm/Models/Enums/TopicOptionCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoForm.Models
+{
+    /// <summary>
+    /// 選項類型
+    /// </summary>
+    public enum TopicOptionCategory
+    {
+        None,
+        左,
+        上,
+        右
+    }
+}
