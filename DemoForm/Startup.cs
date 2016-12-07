ef399a1da5edc0c43edb70af7f0b6e1bde6151ed
Kahou <creator.kahou@gmail.com>
2016-12-07 15:30:36 +0800

    New project

---
+++ DemoForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoForm.Startup))]
+namespace DemoForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
