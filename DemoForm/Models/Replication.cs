0eced66d49801c77bae54b22ae6586967cef74d8
Kahou <creator.kahou@gmail.com>
2016-12-07 16:52:56 +0800

    add 答案 model

---
+++ DemoForm/Models/Replication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DemoForm.Models
+{
+    /// <summary>
+    /// 答案回覆
+    /// </summary>
+    public class Replycation
+    {
+        [DisplayName("題目識別碼")]
+        public Guid TopicId { get; set; }
+
+        [DisplayName("選項識別碼")]
+        public Guid TopicOptionId { get; set; }
+
+        [DisplayName("答案")]
+        public string Answer { get; set; }
+    }
+}
