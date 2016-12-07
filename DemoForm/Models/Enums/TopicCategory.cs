7806413c26fa8ad0a76cb41b2b7b9e8273f13387
Kahou <creator.kahou@gmail.com>
2016-12-07 16:22:23 +0800

    add 題目 model

---
+++ DemoForm/Models/Enums/TopicCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoForm.Models
+{
+    /// <summary>
+    /// 題目類型
+    /// </summary>
+    public enum TopicCategory
+    {
+        單選,
+        複選,
+        簡答,
+        矩陣,
+        相對比較,
+        排序
+    }
+}
---
+++ DemoForm/Models/Topic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DemoForm.Models
+{
+    /// <summary>
+    /// 題目
+    /// </summary>
+    public class Topic
+    {
+        [DisplayName("識別碼")]
+        public Guid Id { get; set; }
+
+        [DisplayName("順序")]
+        public int Sort { get; set; }
+
+        [DisplayName("問題")]
+        public string Question { get; set; }
+
+        [DisplayName("題型")]
+        public TopicCategory Category { get; set; }
+
+        [DisplayName("選項/文字框 數量")]
+        public int Amount { get; set; }
+
+        [DisplayName("選項/文字框內文 上限")]
+        public int Max { get; set; }
+
+        [DisplayName("選項/文字框內文 下限")]
+        public int Min { get; set; }
+
+        [DisplayName("文字框內文格式")]
+        public FormatCategory ContentType { get; set; }
+    }
+}
