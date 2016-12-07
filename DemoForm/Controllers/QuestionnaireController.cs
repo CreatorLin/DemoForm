dee391e8d97cda6c17b78a3223c9e158c121a2cc
Kahou <creator.kahou@gmail.com>
2016-12-07 18:25:53 +0800

    temp

---
+++ DemoForm/Controllers/QuestionnaireController.cs
@@ -0,0 +1,18 @@
+﻿using DemoForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoForm.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        public ActionResult Create()
+        {
+            var viewModel = Enumerable.Range(1, 10).Select(p => new Topic() { Sort = p }).ToArray();
+            return View(viewModel);
+        }
+    }
+}
